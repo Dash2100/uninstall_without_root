b80923455ede6c77501d9ddeff362eaf315decd9
Tony2100 <87257550+Dash2100@users.noreply.github.com>
2025-05-10 00:20:55 +0800

    feat: fix settings page option

--- src/script.js
+++ src/script.js
@@ -2,10 +2,16 @@ const dialogConfirmUninstall = document.querySelector(".dialog-confirm-uninstall
 const dialogWarning = document.querySelector(".dialog-warning");
 const dialogAppInfo = document.querySelector(".dialog-appinfo");
 const dialogDeleteApp = document.querySelector(".dialog-delete-app");
+const dialogADBResponse = document.querySelector(".dialog-adb-response");
 
 const snackbarDisconnected = document.querySelector(".snackbar-disconnected");
 const snackbarConnected = document.querySelector(".snackbar-connected");
+
 const snackbarDeviceId = document.querySelector("#snackbar-device-id");
+
+// settings
+const settingsLanguage = document.getElementById('settings-language');
+const settingsDarkMode = document.getElementById('settings-darkmode');
 
 const pages = {
     appList: document.getElementById('appList'),
@@ -13,7 +19,9 @@ const pages = {
     about: document.getElementById('about')
 };
 
-function switchPage(pageId) {
+let appLang = "zh";
+
+const switchPage = (pageId) => {
     const currentPage = document.querySelector('.page.active');
     const newPage = document.getElementById(pageId);
 
@@ -33,6 +41,29 @@ function switchPage(pageId) {
     }
 }
 
+const uninstallApp = (appId) => {
+    dialogDeleteApp.open = true;
+};
+
+// settings
+
+settingsLanguage.addEventListener('change', (event) => {
+    const selectedLanguage = event.target.value;
+
+    if (selectedLanguage === "") {
+        setTimeout(() => {
+            settingsLanguage.value = appLang;
+        }, 1);
+
+        return;
+    }
+    appLang = selectedLanguage;
+});
+
+settingsDarkMode.addEventListener('change', (event) => {
+    document.body.classList.toggle('mdui-theme-dark');
+});
+
 const initApp = () => {
     // init pqage
     switchPage('appList');
@@ -40,6 +71,5 @@ const initApp = () => {
     // 免責聲明
     // dialogWarning.open = true;
 
-    // Test dialog - can be removed in production
-    // dialogDeleteApp.open = true; 
+    dialogADBResponse.open = true;
 };
